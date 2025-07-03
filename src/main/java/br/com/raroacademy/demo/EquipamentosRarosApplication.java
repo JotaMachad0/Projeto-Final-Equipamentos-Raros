1c56e73f940c5c6743c92fe73e9dfcc964de269b
arthursetragni <arthursetragni@gmail.com>
2025-07-02 21:24:23 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/com/raroacademy/demo/EquipamentosRarosApplication.java
@@ -0,0 +1,13 @@
+package br.com.raroacademy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipamentosRarosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipamentosRarosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/raroacademy/demo/EquipamentosRarosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.raroacademy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipamentosRarosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
